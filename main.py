f2e986efef6b66df368eed96c28a7b49dda3733a
Anuj Harisinghani <anuj.harisinghani2012@gmail.com>
2021-11-03 15:42:59 -0700

    main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import gym
+import mujoco_py
+
+from gym.envs.mujoco import humanoid
+
+env = humanoid.HumanoidEnv()
+obs = env.reset()
+
+for i in range(10000):
+    obs, r, d, info = env.step(env.action_space.sample())
+    env.render()
+
+env.close()
