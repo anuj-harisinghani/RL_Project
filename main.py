0148bbd1b9e18b379ce79e6ca89a5f76215ed03c
Anuj <anuj.harisinghani2012@gmail.com>
2021-11-04 14:01:04 -0700

    a

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import mujoco_py
 
 from gym.envs.mujoco import humanoid
 
-env = humanoid.HumanoidEnv()
+# env = humanoid.HumanoidEnv()
+env = gym.make('Humanoid-v2')
 obs = env.reset()
+
 
 for i in range(10000):
     obs, r, d, info = env.step(env.action_space.sample())
---
+++ map_elites.py
@@ -0,0 +1,9 @@
+import numpy
+import os
+
+
+class MapElites:
+    def __init__(self):
+        pass
+
+    def algorithm(self):
