4ecf41683040d964932bb457607e6eb30f638c3b
Anuj <anuj.harisinghani2012@gmail.com>
2021-11-25 16:32:35 -0800

    multiprocessing pool

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
+import os
+
 import gym
 import mujoco_py
+from multiprocessing import Pool
 
 from gym.envs.mujoco import humanoid
 
-# env = humanoid.HumanoidEnv()
-env = gym.make('Humanoid-v2')
-obs = env.reset()
+
+def main(e):
+    # env = humanoid.HumanoidEnv()
+    env = gym.make('Humanoid-v2')
+    # obs = env.reset()
+
+    # for e in range(50):
+    env.reset()
+    for _ in range(10000):
+        obs, r, d, info = env.step(env.action_space.sample())
+        # env.render()
+        # print(r)
+        if d:
+            print(e, ' done')
+            break
+
+    env.close()
 
 
-for i in range(10000):
-    obs, r, d, info = env.step(env.action_space.sample())
-    env.render()
+if __name__ == '__main__':
+    __file__ = 'main.py'
+    cpu_count = os.cpu_count()
+    pool = Pool(cpu_count)
+    op = [pool.apply_async(main, args=(e,)) for e in range(50)]
 
-env.close()
